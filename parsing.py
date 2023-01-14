e407db0cd8a13f79aef930b27e273969e39737d5
Haseeb Ul Hassan <91841622+Haseeeb21@users.noreply.github.com>
2023-01-14 18:51:02 +0500

    Add files via upload

---
+++ parsing.py
@@ -0,0 +1,26 @@
+
+import xml.etree.ElementTree as ET      #Creating Aias for ease
+        #Using element tree module
+
+mytree = ET.parse("compiler.xml")       #Parsing the xml file
+                    #Split into pieces
+
+for info in mytree.findall("book"):
+
+    print("\n\t\t*-*-*-Displaying Information-*-*-*\n\n"
+        "Book ID:", info.attrib)        #Printing book id attribute
+    auth = info.find("author").text     #Extracting the text from variable
+    titl = info.find("title").text      #given in find func
+    genr = info.find("genre").text      #and storing in variables
+    pric = info.find("price").text
+    pubd = info.find("publish_date").text
+    desc = info.find("description").text
+
+    print("Author Name:", auth,     #Now printing those extracted values
+          "\nTitle:", titl,         #for each book id
+          "\nGenre:", genr,
+          "\nPrice:", pric,
+          "\nPublish Date:", pubd,
+          "\nDescription:", desc)
+
+
